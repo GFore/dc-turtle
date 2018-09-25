6bb93f0159e7c87e1c4ee08da0b6ba299a582330
Greg Foreman <gfore@mindspring.com>
2018-09-25 10:14:23 -0400

    minor changes

--- turt-01.py
+++ turt-01.py
@@ -1,6 +1,7 @@
 from turtle import *
 
 def square(size):
+    color('black')
     forward(size)
     right(90)
     forward(size)
@@ -10,6 +11,7 @@ def square(size):
     forward(size)
 
 def unsquare(size):
+    color('white')
     forward(size)
     left(90)
     forward(size)
@@ -18,22 +20,30 @@ def unsquare(size):
     left(90)
     forward(size)
 
-speed(6)
-square(200)
-right(180)
-speed(6)
-color('white')
-unsquare(200)
+def star():
+    color('green', 'blue')
+    begin_fill()
+    while True:
+        forward(200)
+        left(170)
+        if abs(pos()) < 1:
+            break
+    end_fill()
+
+speed(10)
+for i in range(20):
+    square(200)
+    right(180)
+    unsquare(200)
+    right(25)
 # forward(300)
 # right(180)
-# color('green', 'blue')
-# begin_fill()
-# while True:
-#     forward(200)
-#     left(170)
-#     if abs(pos()) < 1:
-#         break
-# end_fill()
+for i in range(5):
+    star()
+    right(45)
+    penup()
+    forward(200)
+    pendown()
 # done()
 input()
 
