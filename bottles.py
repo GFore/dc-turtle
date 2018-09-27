000215e184cb8e0d8664448193da7e7969723e1f
Greg Foreman <gfore@mindspring.com>
2018-09-27 09:42:04 -0400

    corrects grammar for 1 bottle of beer.

--- bottles.py
+++ bottles.py
@@ -1,4 +1,4 @@
 def takeOneDown(x):
-    print("{0} bottles of beer on the wall, {0} bottles of beer. Take one down, pass it around, {1} bottles of beer on the wall.".format(x, x-1))
+    print("{0} bottle{2} of beer on the wall, {0} bottle{2} of beer.\nTake one down, pass it around, {1} bottle{3} of beer on the wall.".format(x, x-1, 's' if x>1 else '', 's' if x>2 else ''))
 
 for i in range (99, 0, -1): takeOneDown(i)
